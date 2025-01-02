6d809b121ca98fda47cde9714d755d7f95190bf5
pol vazquez <polvazquezalbarracin@gmail.com>
2025-01-02 13:17:56 +0100

    En desarrollo, cambio de pc

--- DungeonMasterApp/Program.cs
+++ DungeonMasterApp/Program.cs
@@ -1,16 +1,11 @@
+using DungeonMasterApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using DungeonMasterApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Registrar los componentes principales
-builder.RootComponents.Add<App>("#app");  // Componente principal de la app
-builder.RootComponents.Add<HeadOutlet>("head::after");  // Permite la inyección de contenido en el head del HTML
-
-// Configuración de HttpClient para consumir APIs
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7109") });
-
-
-// Crear y ejecutar la aplicación Blazor
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorBootstrap();
 await builder.Build().RunAsync();
---
+++ DungeonMasterBackendDotnet/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DungeonMasterBackendDotNet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
